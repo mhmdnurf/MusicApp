fe6ee59f78c7ba819a75a03bdd4c651ae497dd7a
mhmdnurf <zaldebarenz@gmail.com>
2024-12-06 14:41:13 +0700

    add new songs data

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -25,4 +25,40 @@ const track3 = {
   duration: 240,
 };
 
-export const tracks = [track1, track2, track3];
+const track4 = {
+  url: 'https://eqwkxvz36vnuod1e.public.blob.vercel-storage.com/songs/Under%20the%20City%20Stars-9iDm3wQrTHDvw6FlkST3ZYJUY2BrOZ.mp3',
+  title: 'Under the City Stars',
+  artist: 'Middle School, Aso, Aviino',
+  artwork:
+    'https://eqwkxvz36vnuod1e.public.blob.vercel-storage.com/images/ab67616d0000b27392ea33b2d11348d692352a33-IrvvvWMitxWwJ8bouILgAHcLb20Chn.jpeg',
+  duration: 133,
+};
+
+const track5 = {
+  url: 'https://eqwkxvz36vnuod1e.public.blob.vercel-storage.com/songs/Reflection-OOQSp2V31OvDHwcbDsxySyVOlUMV1X.mp3',
+  title: 'Reflection',
+  artist: 'Swørn',
+  artwork:
+    'https://eqwkxvz36vnuod1e.public.blob.vercel-storage.com/images/a1176758100_65-Ly1RUwe982Ih0pSZNVtfzU6hGJntbb.jpeg',
+  duration: 178,
+};
+
+const track6 = {
+  url: 'https://eqwkxvz36vnuod1e.public.blob.vercel-storage.com/songs/Canary%20Forest-frWp2IQ6mEGNBwiH3oACszZ5ZyBfKc.mp3',
+  title: 'Canary Forest',
+  artist: 'Middle School, Aso, Aviino',
+  artwork:
+    'https://eqwkxvz36vnuod1e.public.blob.vercel-storage.com/images/a2013294891_65-6YEH3BKkV7GeRVoEtjOOcHpSaaDODF.jpeg',
+  duration: 137,
+};
+
+const track7 = {
+  url: 'https://eqwkxvz36vnuod1e.public.blob.vercel-storage.com/songs/belonging-mFAO4dphf2J4i0UqtWdX0BkayEymlL.mp3',
+  title: 'belonging',
+  artist: 'Swørn',
+  artwork:
+    'https://eqwkxvz36vnuod1e.public.blob.vercel-storage.com/images/600x600bf-60-bLy83udpQWoElrTf1KkLgyDMykG9xP.jpg',
+  duration: 122,
+};
+
+export const tracks = [track1, track2, track3, track4, track5, track6, track7];
